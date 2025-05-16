dcb1c1adbb982b5a69eef36288c6cb14252bfd1e
ivovila <ivilamowski@itba.edu.ar>
2025-05-16 10:32:30 -0300

    Function name errors

--- src/main/c/EntryPoint.c
+++ src/main/c/EntryPoint.c
@@ -20,8 +20,8 @@ const int main(const int count, const char ** arguments) {
 	initializeBisonActionsModule();
 	initializeSyntacticAnalyzerModule();
 	initializeAbstractSyntaxTreeModule();
-	initializeCalculatorModule();
-	initializeGeneratorModule();
+//	initializeCalculatorModule();
+//	initializeGeneratorModule();
 
 	// Logs the arguments of the application.
 	for (int k = 0; k < count; ++k) {
@@ -60,8 +60,8 @@ const int main(const int count, const char ** arguments) {
 	logDebugging(logger, "Releasing AST resources...");
 	releaseProgram(program);
 	logDebugging(logger, "Releasing modules resources...");
-	shutdownGeneratorModule();
-	shutdownCalculatorModule();
+//	shutdownGeneratorModule();
+//	shutdownCalculatorModule();
 	shutdownAbstractSyntaxTreeModule();
 	shutdownSyntacticAnalyzerModule();
 	shutdownBisonActionsModule();
